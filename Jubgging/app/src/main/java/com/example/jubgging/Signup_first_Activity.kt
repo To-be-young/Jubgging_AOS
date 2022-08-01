c1426a6713076cfce3b7f85a25414c5a41248ea7
lh99j <grand2181@gmail.com>
2022-08-01 16:54:04 +0900

    회원가입 - 가입정보 수정

--- Jubgging/app/src/main/java/com/example/jubgging/Signup_first_Activity.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/Signup_first_Activity.kt
@@ -3,7 +3,7 @@ package com.example.jubgging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegisterActivity1 : AppCompatActivity() {
+class Signup_first_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_first)
