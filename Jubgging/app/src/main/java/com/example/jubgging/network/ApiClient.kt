b000b5dc975ef3704c84b683b9f83fd2daa357de
getupminaaa <getupmina0405@gmail.com>
2022-08-10 15:37:38 +0900

    api 통신 구조 생성 및 gitignore 업데이트

--- Jubgging/app/src/main/java/com/example/jubgging/network/ApiClient.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/network/ApiClient.kt
@@ -1,4 +1,32 @@
 package com.example.jubgging.network
 
-class ApiClient {
+import com.example.jubgging.network.C.C
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private val client = OkHttpClient.Builder().apply {
+        addInterceptor(ApiInterceptor())
+    }.build()
+    var gson = GsonBuilder().setLenient().create()
+    private val retrofit by lazy {
+        Retrofit.Builder().baseUrl(C.BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
+    }
+
+    val api: ApiInterface by lazy {
+        retrofit.create(ApiInterface::class.java)
+    }
+}
+
+interface ServerResult {
+    fun isSuccess(): Boolean
+    fun code(): String
+    fun errorMessage(): String?
 }
--- Jubgging/app/src/main/java/com/example/jubgging/network/ApiInterceptor.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/network/ApiInterceptor.kt
@@ -1,4 +1,12 @@
 package com.example.jubgging.network
 
-class ApiInterceptor {
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ApiInterceptor:Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+        return chain.proceed(builder.build())
+    }
+
 }
