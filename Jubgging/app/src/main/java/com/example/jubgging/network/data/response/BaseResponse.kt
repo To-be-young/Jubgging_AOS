9842adaa2683f047dac6cf990b62ea31bc0bb51d
getupminaaa <getupmina0405@gmail.com>
2022-08-27 18:12:08 +0900

    BaseResponse 수정

--- Jubgging/app/src/main/java/com/example/jubgging/network/data/response/BaseResponse.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/network/data/response/BaseResponse.kt
@@ -1,20 +1,22 @@
 package com.example.jubgging.network.data.response
+
 import com.example.jubgging.network.ServerResult
 
-open class BaseResponse <T>(
+open class BaseResponse<T>(
     var code: Int = -1,
+    var success: Boolean = false,
     var message: String? = null,
-    val data: T
-): ServerResult {
+    val data: T,
+) : ServerResult {
     override fun isSuccess(): Boolean {
-        return code == 0
+        return success && code == 0
     }
 
-    override fun code():String{
+    override fun code(): String {
         return code.toString()
     }
 
-    override fun errorMessage():String?{
+    override fun errorMessage(): String? {
         return message
     }
 }
