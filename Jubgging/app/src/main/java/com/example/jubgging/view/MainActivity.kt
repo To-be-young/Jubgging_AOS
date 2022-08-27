7e8d8c269642e961964ecd1c3795d86ad674ac70
lh99j <grand2181@gmail.com>
2022-08-28 00:40:36 +0900

    Jetpack Bottom NavigationUI 구현

--- Jubgging/app/src/main/java/com/example/jubgging/view/MainActivity.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/view/MainActivity.kt
@@ -4,42 +4,52 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.jubgging.R
 import com.example.jubgging.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.main = this
 
+        initNavigation()
 
-        binding.bottomNavigationView.setOnItemSelectedListener(this)
-        supportFragmentManager.beginTransaction().add(R.id.main_fl, HomeFragment()).commit()
+//        binding.bottomNavigationView.setOnItemSelectedListener(this)
+//        supportFragmentManager.beginTransaction().add(R.id.main_fl, HomeFragment()).commit()
 
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        //추후 Jetpack의 NavigationUI로 수정할 예정
-        when (item.itemId) {
-            R.id.main_community -> {
-                supportFragmentManager.beginTransaction().replace(R.id.main_fl, CommunityFragment())
-                    .commitAllowingStateLoss()
-                return true
-            }
-            R.id.main_home -> {
-                supportFragmentManager.beginTransaction().replace(R.id.main_fl, HomeFragment())
-                    .commitNowAllowingStateLoss()
-                return true
-            }
-            R.id.main_my -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_fl, MyFragment()).commitAllowingStateLoss()
-                return true
-            }
-            else -> return false
-        }
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, findNavController(R.id.navi_host))
     }
+
+
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        //추후 Jetpack의 NavigationUI로 수정할 예정
+//        when (item.itemId) {
+//            R.id.main_community -> {
+//                supportFragmentManager.beginTransaction().replace(R.id.main_fl, CommunityFragment())
+//                    .commitAllowingStateLoss()
+//                return true
+//            }
+//            R.id.main_home -> {
+//                Navigation.findNavController(binding.root).navigate(R.id.action_fragmentOne_to_fragmentTwo)
+//                return true
+//            }
+//            R.id.main_my -> {
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.main_fl, MyFragment()).commitAllowingStateLoss()
+//                return true
+//            }
+//            else -> return false
+//        }
+//    }
 }
