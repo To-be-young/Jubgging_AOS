2e8f92e3030b0b32bb74ecdf58ad828caa738f28
lh99j <grand2181@gmail.com>
2022-08-27 20:04:31 +0900

    카카오맵 커스텀 말풍선

---
+++ Jubgging/app/src/main/java/com/example/jubgging/view/Ballon_layout.kt
@@ -0,0 +1,12 @@
+package com.example.jubgging.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jubgging.R
+
+class Ballon_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ballon_layout)
+    }
+}
