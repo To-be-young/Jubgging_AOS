fbf91febbf9b57a7dbe480bdcb15dd7600157102
getupminaaa <getupmina0405@gmail.com>
2022-08-28 17:38:22 +0900

    splash screen 적용 및 app_icon 적용

--- Jubgging/app/src/main/java/com/example/jubgging/view/MainActivity.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/view/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.jubgging.view
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.jubgging.R
 import com.example.jubgging.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,34 +19,10 @@ class MainActivity : AppCompatActivity() {
 
         initNavigation()
 
-//        binding.bottomNavigationView.setOnItemSelectedListener(this)
-//        supportFragmentManager.beginTransaction().add(R.id.main_fl, HomeFragment()).commit()
-
     }
 
     private fun initNavigation() {
-        NavigationUI.setupWithNavController(binding.bottomNavigationView, findNavController(R.id.navi_host))
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,
+            findNavController(R.id.navi_host))
     }
-
-
-//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        //추후 Jetpack의 NavigationUI로 수정할 예정
-//        when (item.itemId) {
-//            R.id.main_community -> {
-//                supportFragmentManager.beginTransaction().replace(R.id.main_fl, CommunityFragment())
-//                    .commitAllowingStateLoss()
-//                return true
-//            }
-//            R.id.main_home -> {
-//                Navigation.findNavController(binding.root).navigate(R.id.action_fragmentOne_to_fragmentTwo)
-//                return true
-//            }
-//            R.id.main_my -> {
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.main_fl, MyFragment()).commitAllowingStateLoss()
-//                return true
-//            }
-//            else -> return false
-//        }
-//    }
 }
