24775081b6dd5fbf114e647fb6114c420a8f5c3c
getupminaaa <getupmina0405@gmail.com>
2022-09-22 02:04:12 +0900

    community_create_activity ui 생성

--- Jubgging/app/src/main/java/com/example/jubgging/view/CommunityCreateActivity.kt
+++ Jubgging/app/src/main/java/com/example/jubgging/view/CommunityCreateActivity.kt
@@ -1,28 +1,21 @@
 package com.example.jubgging.view
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.jubgging.R
-import com.example.jubgging.databinding.ActivityCommunityGroupListBinding
+import com.example.jubgging.databinding.ActivityCommunityCreateBinding
 import com.example.jubgging.viewmodel.CommunityViewModel
 
-class CommunityListActivity:AppCompatActivity() {
-    private lateinit var binding: ActivityCommunityGroupListBinding
+class CommunityCreateActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCommunityCreateBinding
     private val viewModel: CommunityViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_community_group_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_community_create)
         binding.lifecycleOwner = this
         binding.communityVm = viewModel
-
-        binding.cglCreateCommunityBtn.setOnClickListener {
-            //이동
-            val intent = Intent(this, CommunityCreateActivity::class.java)
-            startActivity(intent)
-        }
     }
+
 }
