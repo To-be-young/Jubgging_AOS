3facc6f1de0e5d42e19e93eda829eb993f709668
getupminaaa <getupmina0405@gmail.com>
2022-09-22 03:06:30 +0900

    community_group_detail ui 생성

---
+++ Jubgging/app/src/main/java/com/example/jubgging/view/CommunityDetailActivity.kt
@@ -0,0 +1,22 @@
+package com.example.jubgging.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.jubgging.R
+import com.example.jubgging.databinding.ActivityCommunityDetailBinding
+import com.example.jubgging.viewmodel.CommunityViewModel
+
+class CommunityDetailActivity:AppCompatActivity() {
+    private lateinit var binding: ActivityCommunityDetailBinding
+    private val viewModel: CommunityViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_community_detail)
+        binding.lifecycleOwner = this
+        binding.communityVm = viewModel
+
+    }
+}
