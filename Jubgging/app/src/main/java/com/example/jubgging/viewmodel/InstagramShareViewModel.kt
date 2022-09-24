42188ed8ce3c70f2f260b9c440531359b6ab3f43
hye219 <jhy_0219@daum.net>
2022-09-24 11:01:16 +0900

    앱 내 사진 가져오기 및 인스타그램 스토리 공유 구현

---
+++ Jubgging/app/src/main/java/com/example/jubgging/viewmodel/InstagramShareViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.jubgging.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.jubgging.R
+
+class InstagramShareViewModel : ViewModel() {
+    //뷰에 나타낼 값들. 라이브데이터 형식
+    var image = MutableLiveData<Int>()
+    var background = MutableLiveData<Int>()
+    val content = MutableLiveData<String>()
+
+//    //이미지, 배경 리스트들
+//    private var imageList = mutableListOf<Int>()
+//    private var backgroundList = mutableListOf<Int>()
+
+    init{
+        image.value = R.drawable.android
+
+        background.value = R.color.white
+
+        content.value = ""
+
+
+    }
+
+}
