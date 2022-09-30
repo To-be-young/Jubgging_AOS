a25092ef7002bafb7f5649c0da0513bde10b65e4
hye219 <jhy_0219@daum.net>
2022-09-30 18:32:43 +0900

    오픈소스 라이선스 고지 완료

---
+++ Jubgging/app/src/main/java/com/example/jubgging/view/OssNoticeActivity.kt
@@ -0,0 +1,23 @@
+package com.example.jubgging.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jubgging.databinding.ActivityOssNoticeBinding
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+
+
+class OssNoticeActivity : AppCompatActivity() {
+    private val binding: ActivityOssNoticeBinding by lazy {
+        ActivityOssNoticeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnOpensourceLicense.setOnClickListener {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        }
+    }
+}
